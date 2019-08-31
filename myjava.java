f600e1f95c0ee4e2a7cfaaac7e79e0402250fdcf
zeng <2936642799@qq.com>
2019-08-31 20:57:54 +0800

    规划和

---
+++ myjava.java
@@ -0,0 +1,12 @@
+public class ItemsMapperTest {
+    @Test
+    public void testFindItemsById(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+
+        ItemsMapper itemsMapper = applicationContext.getBean(ItemsMapper.class);
+        Items items = itemsMapper.findById(1);
+        System.out.println(items);
+
+    }
+
+}
